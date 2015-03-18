d60c2f29c6bd95cd0be97cfff20251ec135c913c
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-18 17:58:52 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- fn.ex
+++ fn.ex
@@ -5,6 +5,8 @@ defmodule Fn do
   provides factorial
   """
 
+  @spec f(integer) :: integer
+
   # Define prototype
   @doc """
   compute factorial of given number x
@@ -19,11 +21,17 @@ defmodule Fn do
   def f(x) do
     x * f(x-1)
   end
+
+  def g(a, b) do
+    a + b
+  end
 end
 
-IO.puts M.f(3)
+IO.puts Fn.f(3)
 1 |> IO.puts
 f = fn x -> x + 1 end
 g = &(&1 + 1)
 f.(1) |> IO.puts
 g.(1) |> IO.puts
+
+Fn.g(1, 2) |> IO.puts 
